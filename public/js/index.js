260563a2a7c9f6c2cec3068c2531ba86757c833e
yanolsh <yanolsh@bu.edu>
2017-05-04 20:19:45 -0400

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,27 @@
    });
    socket.on('newMessage', function(message) {
        console.log('newMessage', message);
+       var li = jQuery('<li></li>');
+       li.text(`${message.from}: ${message.text}`);
+
+       jQuery('#messages').append(li);
    })
    socket.on('disconnect', function() {
        console.log('Disconnected from server');
+   });
+   //    socket.emit('createMessage', {
+   //        from: 'Frank',
+   //        text: 'Hi'
+   //    }, function(data) {
+   //        console.log('Got it', data);
+   //    });
+   jQuery('#message-form').on('submit', function(e) {
+       e.preventDefault();
+
+       socket.emit('createMessage', {
+           from: 'User',
+           text: jQuery('[name=message]').val()
+       }, function() {
+
+       });
    });
